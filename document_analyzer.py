4d002403ce4c44f2d6e77e2fb1817e14d6c65fdc
Trung Huynh <trung.huynh@sjsu.edu>
2022-03-09 19:11:46 -0800

    almost done with 2, no lowercasing

---
+++ document_analyzer.py
@@ -0,0 +1,45 @@
+doc = open("document.txt", "r")
+doc_data = doc.read()
+#attempt to remove as many common special characters in the document
+#assuming pronouns are not removed
+doc_list = doc_data.replace('\n', ' ').replace(',', '').replace(':', '').replace('.', ' ').replace(';', '').replace('(', '').replace(')', '').replace('“', '').replace('”', '').split(' ')
+#print(doc_list)
+list_of_words = [] #list of unique words
+list_of_wc = [] #list of word count corresponding to list_of_words
+
+#first, we need to find all the unique words
+for i in range(len(doc_list)): #iterate through words from document
+    is_unique = True
+    for j in range(len(list_of_words)): #check if words are unique
+        if doc_list[i] == list_of_words[j]:
+            #word is already in list of unique words, increment word count
+            is_unique = False
+            list_of_wc[j] = list_of_wc[j] + 1
+    if is_unique and doc_list[i] != '' and doc_list[i] != ' ':
+        #add word to list 
+        list_of_words.append(doc_list[i])
+        list_of_wc.append(1)
+        
+#now to sort by most frequent words
+i = 0; #index 0 of the list
+while i < len(list_of_words): #iterate through list of unique words
+    j = 0
+    while j < len(list_of_words) - i - 1:
+        if list_of_wc[j] < list_of_wc[j+1]: #sort by descending order
+            #swapping both the word and word count positions
+            temp = list_of_wc[j]
+            list_of_wc[j] = list_of_wc[j+1]
+            list_of_wc[j+1] = temp
+            
+            temp = list_of_words[j]
+            list_of_words[j] = list_of_words[j+1]
+            list_of_words[j+1] = temp
+        j = j + 1
+    i = i + 1
+
+#prelim display
+for count in range(len(list_of_words)):
+    print(list_of_words[count] + ": " + str(list_of_wc[count]))
+
+        
+doc.close()
